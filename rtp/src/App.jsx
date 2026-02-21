e2ea3d898722e72ad4bb21a20d54c8d91aaeda00
harshu-x <harshalpet07@gmail.com>
2026-02-22 01:15:29 +0530

    API Calls Initiated

--- rtp/src/App.jsx
+++ rtp/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { fetchPhotos } from './api/mediaApi'
 
 const App = () => {
+  
   return (
-    <div>
-      <h1 className='h-screen w-full bg-gray-850'>This is a heading..</h1>
+    <div className='h-screen w-full bg-gray-850'>
+      
+
+      <button className='bg-red-600' 
+      onClick={()=>{
+        fetchPhotos('cat')
+      }}>Get Photos</button>
     </div>
   )
 }
